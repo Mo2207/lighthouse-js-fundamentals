982502d6ac3332fcd656b3edc6069765c7c30dce
Mo2207 <gabemorris1042@gmail.com>
2022-01-17 00:03:06 -0400

    added comments and rearranged conditionals more efficiently

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,12 +1,12 @@
 
 for (let i = 100; i <= 200; i++) {
-  if (i % 3 === 0 && !(i % 4 === 0)) {
-    console.log("Loopy");
-  } else if (i % 4 === 0 && !(i % 3 === 0)) {
-    console.log("Lighthouse");
-  } else if (i % 4 === 0 && i % 3 === 0) {
-    console.log("LoopyLighthouse");
+  if (i % 4 === 0 && i % 3 === 0) {
+    console.log("LoopyLighthouse"); // if divisible by both 3 $ 4
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse"); // if divisible by 4 only
+  } else if (i % 3 === 0) {
+    console.log("Loopy"); // if divisible by 3 only
   } else {
-    console.log(i);
+    console.log(i); // log number
   }
 }
